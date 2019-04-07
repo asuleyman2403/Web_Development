3d735b9e0eb99cb6329c70c08b11d2edc6064957
asuleyman2403 <asuleyman2403@gmail.com>
2019-04-07 23:27:38 +0600

    lab11

---
+++ week_11/todo-front/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {ProviderService} from './services/provider.service';
+import { ViewerComponent } from './components/viewer/viewer.component';
+import {HttpClient, HttpHandler} from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ViewerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [ProviderService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
